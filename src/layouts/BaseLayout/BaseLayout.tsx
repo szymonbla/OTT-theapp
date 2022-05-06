91532bd7159c5df5d2cc8c9aa2e357900c0a2c40
Kuchar <sz.blazynski@gmail.com>
2022-05-06 13:02:37 +0200

    AB#50 Implement logo with animation

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,19 +1,22 @@
 import { ReactNode } from 'react';
-import { Box } from '@mui/material';
+import { Box, SxProps } from '@mui/material';
 
 interface BaseLayoutProps {
   children: ReactNode;
+  sx?: SxProps;
 }
 
-export const BaseLayout = ({ children }: BaseLayoutProps) => {
+export const BaseLayout = ({ children, sx }: BaseLayoutProps) => {
   return (
     <Box
-      sx={(theme) => ({
+      sx={{
+        ...sx,
         width: '100vw',
         height: '100vh',
         position: 'relative',
-        background: `linear-gradient(180deg, ${theme.palette.secondary.main}, ${theme.palette.background.default})`
-      })}
+        background: (theme) =>
+          `linear-gradient(180deg, ${theme.palette.secondary.main}, ${theme.palette.background.default})`
+      }}
     >
       {children}
     </Box>
